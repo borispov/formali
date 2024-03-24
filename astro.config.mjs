52e9040b0a11bc83063562b466d1a67784f8ea2c
Boris Povolotsky <boristofu@gmail.com>
2024-03-25 00:18:31 +0200

    wip: demo form almost done with alpinejs.. figuring out whether to use AlpineJS or Astro for the presentation of the forms

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,20 @@ import { defineConfig } from 'astro/config';
 import UnoCSS from 'unocss/astro';
 import { presetUno } from 'unocss';
 import svelte from "@astrojs/svelte";
+import alpine from '@astrojs/alpinejs'
 
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  integrations: [UnoCSS({
-    presets: [presetUno()],
-    injectReset: true
-  }), svelte()],
+  integrations: [
+    UnoCSS({
+      presets: [presetUno()],
+      injectReset: true
+    }), 
+    svelte(),
+    alpine(),
+  ],
   adapter: netlify()
 });
