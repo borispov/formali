c035c19446f70e86787cb9e802eb6f9cef20f453
Boris Povolotsky <boristofu@gmail.com>
2024-03-27 12:18:54 +0200

    attempt to fix build issues on deployment to netlify

--- .netlify/functions-internal/ssr/chunks/info_DuW2pjaA.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/info_BpAn5ULw.mjs');
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/prerender_D5qV3WWL.mjs
+++
@@ -1,7 +0,0 @@
-const noop = () => {};
-export const a = noop;
-export const b = noop;
-export const d = noop;
-export const i = noop;
-export const s = noop;
-export const t = noop;
